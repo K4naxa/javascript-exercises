2f3cf50e7fafb00995c792062aba38c36f518430
K4naxa <K4na@iki.fi>
2022-11-12 15:03:04 +0200

    completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitString = str.split("")
+    let reverseArray = splitString.reverse();
+    let reversed = reverseArray.join("")
+    return reversed;
 };
 
 // Do not edit below this line
