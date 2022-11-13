cf0f9093d2184412d26b9350e9d658e8133baa9c
K4naxa <K4na@iki.fi>
2022-11-13 10:25:20 +0200

    completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    typeof num1 !== "number" ||
+    typeof num2 !== "number" ||
+    num1 < 0 ||
+    num2 < 0
+  )
+    return "ERROR";
 
+  let answer = 0;
+
+  if (num1 <= num2) {
+    for (num1; num1 <= num2; num1++) {
+      answer += num1;
+    }
+    return answer;
+  } else if (num1 > num2) {
+    for (num2; num2 <= num1; num2++) {
+      answer += num2;
+    }
+    return answer;
+  }
 };
+
+console.log(sumAll(-1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
